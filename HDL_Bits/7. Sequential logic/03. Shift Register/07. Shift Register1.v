84752e77ceab985ad5dfb11328f6e95fc87978f4
ValanRaj-23 <valanrajs.2364@gmail.com>
2025-02-15 09:22:29 +0530

    07. Shift Register1.v

--- HDL_Bits/7. Sequential logic/03. Shift Register/07. Shift Register1.v
+++ HDL_Bits/7. Sequential logic/03. Shift Register/07. Shift Register1.v
@@ -1,1 +1,15 @@
+module top_module (
+    input clk,
+    input resetn,   // synchronous reset
+    input in,
+    output out);
 
+    reg [3:0] temp;
+    
+    always@(posedge clk )begin
+        if(!resetn) temp <= 0;
+    	else  temp <= {temp[2:0], in};
+    end
+    
+	assign out = temp[3];
+endmodule
