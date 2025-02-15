d614fc07b3c962a5566f83dcddcae795a645bece
ValanRaj-23 <valanrajs.2364@gmail.com>
2025-02-15 09:25:03 +0530

    09. 3-Input LUT.v

---
+++ HDL_Bits/7. Sequential logic/03. Shift Register/09. 3-Input LUT.v
@@ -0,0 +1,16 @@
+module top_module (
+    input clk,
+    input enable,
+    input S,
+    input A, B, C,
+    output Z ); 
+
+    reg [7:0] temp;
+ 
+    always@(posedge clk)
+        if(enable)begin
+            temp <= {temp[6:0],S};
+        end
+    assign Z = temp[{A, B, C}];
+    
+endmodule
