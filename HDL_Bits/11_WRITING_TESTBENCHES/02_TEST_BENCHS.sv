2eac9d4e502c9be2d4c0e095f57f1d21b7cdd1e9
ValanRaj-23 <valanrajs.2364@gmail.com>
2025-03-13 23:29:19 +0530

    02_TEST_BENCHS.sv

---
+++ HDL_Bits/11_WRITING_TESTBENCHES/02_TEST_BENCHS.sv
@@ -0,0 +1,19 @@
+module top_module ( output reg A, output reg B );
+    
+    initial begin
+            A = 0;
+        	#10;
+        	A = 1;
+        	#10;
+        	A = 0;       
+    end
+    
+    initial begin
+            B = 0;
+        	#15;
+        	B = 1;
+        	#25;
+        	B = 0;
+    end
+
+endmodule
